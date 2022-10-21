20a76c96a16f5d0b0b6bdf47cf3b8f1246cd6ec1
bet97 <betka.kasanicka@gmail.com>
2022-10-21 09:38:44 +0300

    -small changes

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="main">
+    <font-awesome-icon :icon="['fas', 'remove']" class="closeIconMobile" />
     <h1>Bucket list</h1>
     <table>
       <thead>
         <tr>
           <td class="head">Done</td>
+          <td></td>
           <td></td>
           <td></td>
           <td></td>
@@ -17,19 +19,23 @@
       <tbody>
         <tr v-for="item in bucketList" :key="item.uuid">
           <td>
-            <input
-              type="checkbox"
-              @click="updateItemCheck(item.uuid, (item.done = !item.done))"
-            />
+            <label class="container">
+              <input
+                type="checkbox"
+                @click="updateItemCheck(item.uuid, (item.done = !item.done))"
+              />
+              <span class="checkmark"></span>
+            </label>
           </td>
           <td>{{ item.description }}</td>
           <td></td>
           <td></td>
           <td></td>
           <td></td>
+          <td></td>
           <td>{{ item.do_before }}</td>
-          <td>
-            <div class="moveright">
+          <td class="optionButtonDialog">
+            <div>
               <font-awesome-icon
                 :icon="['fas', 'ellipsis']"
                 class="dots"
@@ -42,7 +48,7 @@
                 <font-awesome-icon
                   :icon="['fas', 'remove']"
                   @click="item.isVisible = !item.isVisible"
-                  id="closeIconMobile"
+                  class="closeIconMobile"
                 />
               </div>
               <div class="popUp">
@@ -72,11 +78,11 @@
       <hr />
     </div>
   </div>
+  {{ bucketList }}
 </template>
 
 <script>
 import axios from "axios";
-import { API_KEY, baseUrl } from "./constants";
 export default {
   data() {
     return {
@@ -92,8 +98,8 @@ export default {
   methods: {
     fetchData() {
       axios
-        .get(`${baseUrl}item`, {
-          headers: { "api-key": API_KEY },
+        .get(`${import.meta.env.VITE_BASE_URL}item`, {
+          headers: { "api-key": import.meta.env.VITE_APP_API_KEY },
         })
         .then(
           (response) =>
@@ -112,12 +118,12 @@ export default {
     updateItemCheck(id, done) {
       axios
         .put(
-          `${baseUrl}item/${id}`,
+          `${import.meta.env.VITE_BASE_UR}item/${id}`,
           {
             done: done,
           },
           {
-            headers: { "api-key": API_KEY },
+            headers: { "api-key": import.meta.env.VITE_APP_API_KEY },
           }
         )
         .then((response) => response)
@@ -131,8 +137,8 @@ export default {
     deleteItem(id) {
       if (confirm("Do you really want to delete this item?")) {
         axios
-          .delete(`${baseUrl}item/${id}`, {
-            headers: { "api-key": API_KEY },
+          .delete(`${import.meta.env.VITE_BASE_UR}item/${id}`, {
+            headers: { "api-key": import.meta.env.VITE_APP_API_KEY },
           })
           .then((response) => response)
           .catch((error) => {
@@ -168,36 +174,44 @@ export default {
   letter-spacing: 0.25px;
   color: #002844;
 }
+/* HTML elements css*/
 td {
   padding: 10px;
   font-size: 0.875em;
   font-style: normal;
 }
-.head {
-  font-size: 0.6em;
-}
-
 button {
-  background-color: #002844;
+  background: #002844;
+  border: #002844;
   color: white;
-  padding: 11px;
+  padding: 8px;
   border-radius: 8px;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  margin-bottom: -50px;
+  justify-content: center;
+  width: 50%;
 }
-
 hr {
   border: 1px solid #b5c5d0;
 }
-
 h1,
 h3 {
   padding-left: 5%;
   font-weight: medium;
 }
-
 button:hover {
   background: #4a575f;
 }
+hr {
+  width: 100%;
+}
 
+/* classes */
+.head {
+  font-size: 0.6em;
+}
 .dots:hover {
   border-radius: 50%;
   background-color: rgba(214, 224, 232, 1);
@@ -231,45 +245,110 @@ button:hover {
   padding: 10px;
 }
 
-/* toto nefunguje ?? */
-input:checked {
-  background: #002844;
-  border: 1px solid #002844;
-  border-radius: 4px;
-}
-
-input:hover {
-  background: #f5f5f5;
-  border: 1px solid #67889f;
-  border-radius: 4px;
-}
-
 .onlyOnMobile {
   display: none;
 }
 
 .onlyOnDesktop {
   display: block;
-  text-align: right;
 }
 
-hr {
-  width: 100%;
+.closeIconMobile {
+  color: rgba(153, 169, 180, 1);
+  position: absolute;
+  top: 15px;
+  right: 15px;
 }
 
-#deleteIcon {
+#deleteBucketItem {
   color: #ff5093;
 }
-.classChange {
-  color: #ff5093;
+#deleteBucketItem:hover {
+  color: #893e5b;
 }
-/* optimize for mobile deices */
+
+/* w3schools checkbox */
+
+/* The container */
+.container {
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default checkbox */
+.container input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 20px;
+  width: 20px;
+  background: #ffffff;
+  border: 1px solid #67889f;
+  border-radius: 4px;
+}
+
+/* On mouse-over, add a grey background color */
+.container:hover input ~ .checkmark {
+  background: #f5f5f5;
+  border: 1px solid #67889f;
+}
+
+/* When the checkbox is checked, add a blue background */
+.container input:checked ~ .checkmark {
+  background: #002844;
+  border: 1px solid #002844;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.container input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the checkmark/indicator */
+.container .checkmark:after {
+  left: 6.3px;
+  top: 2px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+
+/* optimize for mobile devices */
 
 @media only screen and (max-width: 600px) {
   .main {
     width: 100%;
     padding: 0;
     font-size: 1em;
+    overflow: hidden;
   }
   .showMore {
     margin-bottom: -5px;
@@ -293,12 +372,6 @@ hr {
   .popUp {
     padding: 12px;
     margin-left: 20px;
-  }
-  #closeIconMobile {
-    color: rgba(153, 169, 180, 1);
-    position: absolute;
-    top: 10px;
-    right: 10px;
   }
 }
 </style>
